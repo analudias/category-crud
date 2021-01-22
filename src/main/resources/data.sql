e275291ed7e3dc77f364d5f768b7d4ff59452be0
Ana Luisa Dias <analudias@live.com>
2021-01-21 22:08:53 -0300

    Database seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (NAME, CREATED_AT) VALUES ('Livros', NOW());
+INSERT INTO tb_category (NAME, CREATED_AT) VALUES ('Eletrônicos', NOW());
+INSERT INTO tb_category (NAME, CREATED_AT) VALUES ('Eletrodomésticos', NOW());
+INSERT INTO tb_category (NAME, CREATED_AT) VALUES ('Jogos PS4', NOW());
+INSERT INTO tb_category (NAME, CREATED_AT) VALUES ('Móveis', NOW());
