494cc539e0dc67fddfcfabb223098ef02de7c509
Ana Luisa Dias <analudias@live.com>
2021-01-21 21:26:30 -0300

    First commit

---
+++ src/main/java/com/purplepizza/categorycrud/CategorycrudApplication.java
@@ -0,0 +1,13 @@
+package com.purplepizza.categorycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorycrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorycrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purplepizza/categorycrud/CategorycrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purplepizza.categorycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategorycrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
